98cb50e8e3f7537274aa591e4a1d8d7e1d337034
DannyHumphrey <175.danny@gmail.com>
2025-07-12 08:19:02 +0100

    Add navigation with stack and screens

---
+++ screens/OutboxScreen.tsx
@@ -0,0 +1,10 @@
+import { ThemedView } from '@/components/ThemedView';
+import { ThemedText } from '@/components/ThemedText';
+
+export default function OutboxScreen() {
+  return (
+    <ThemedView style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <ThemedText type="title">Outbox</ThemedText>
+    </ThemedView>
+  );
+}
---
+++ screens/SentScreen.tsx
@@ -0,0 +1,10 @@
+import { ThemedView } from '@/components/ThemedView';
+import { ThemedText } from '@/components/ThemedText';
+
+export default function SentScreen() {
+  return (
+    <ThemedView style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <ThemedText type="title">Sent</ThemedText>
+    </ThemedView>
+  );
+}
